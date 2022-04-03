d0a95dffab8b43d91e405d883a58ef36eb5145a7
DeathVenom54 <fdragon602@gmail.com>
2022-04-03 13:06:59 +0530

    setup logging

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,9 @@ module github.com/DeathVenom54/doto-backend
 
 go 1.17
 
-require github.com/go-chi/chi v1.5.4 // indirect
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20220403020550-483a9cbc67c0 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"github.com/DeathVenom54/doto-backend/router"
+	"github.com/joho/godotenv"
+	logger "github.com/sirupsen/logrus"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Listening for requests on :3000")
-	err := http.ListenAndServe(":3000", router.Router)
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("Error loading .env file:\n%s\n", err)
 	}
+
+	err = setupLogger()
+	if err != nil {
+		log.Fatalf("Error while setting up logger:\n%s\n", err)
+	}
+
+	logger.Info("Listening for requests on :3000")
+	err = http.ListenAndServe(":3000", router.Router)
+	if err != nil {
+		logger.Fatalln(err)
+	}
+}
+
+func setupLogger() error {
+	environment := os.Getenv("ENVIRONMENT")
+
+	if environment == "PROD" {
+		logger.SetFormatter(&logger.JSONFormatter{
+			PrettyPrint: true,
+		})
+	} else {
+		logger.SetFormatter(&logger.TextFormatter{
+			FullTimestamp: true,
+		})
+	}
+
+	return nil
 }
