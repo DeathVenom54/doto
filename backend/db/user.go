bc0549882f764b54d7af7201c1c8ae67883b92b8
DeathVenom54 <fdragon602@gmail.com>
2022-04-08 17:24:16 +0530

    send jwt cookie with signup

--- backend/db/user.go
+++ backend/db/user.go
@@ -8,20 +8,28 @@ import (
 
 type User struct {
 	gorm.Model
+	ID       uint   `gorm:"primaryKey"`
 	Username string `gorm:"uniqueIndex"`
 	Email    string `gorm:"uniqueIndex"`
 	Password string
 }
 
-func CreateUser(user *User) error {
+func CreateUser(user *User) (*User, error) {
 	user.ID = snowflake.GenerateUint()
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 12)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	user.Password = string(hash)
 
 	result := DB.Create(&user)
-	return result.Error
+	return user, result.Error
+}
+
+func GetUserById(id uint) (*User, error) {
+	user := &User{ID: id}
+	res := DB.First(user)
+
+	return user, res.Error
 }
--- backend/handlers/auth_signup.go
+++ backend/handlers/auth_signup.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/DeathVenom54/doto-backend/db"
+	"github.com/DeathVenom54/doto-backend/token"
 	"log"
 	"net/http"
 	"regexp"
@@ -11,6 +12,17 @@ import (
 )
 
 func Signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.RemoteAddr)
+	// check if already logged in
+	if alreadyLoggedIn := token.VerifyToken(r.Header.Get("user")); alreadyLoggedIn {
+		w.WriteHeader(200)
+		_, err := w.Write([]byte("success"))
+		if err != nil {
+			log.Printf("Error while writing response at /auth/signup\n%s", err)
+		}
+		return
+	}
+
 	var userData db.User
 	err := json.NewDecoder(r.Body).Decode(&userData)
 	if err != nil {
@@ -24,11 +36,31 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = db.CreateUser(&userData)
+	// create user in database
+	var createdUser *db.User
+	createdUser, err = db.CreateUser(&userData)
 	if err != nil {
 		handleError(err, w, http.StatusBadRequest)
 		return
 	}
+
+	// write jwt token
+	claims := &token.AuthClaims{
+		ID:       createdUser.ID,
+		Username: createdUser.Username,
+	}
+	jwtToken, err := token.CreateToken(claims)
+	if err != nil {
+		handleError(err, w, http.StatusBadRequest)
+		return
+	}
+	cookie := http.Cookie{
+		Name:     "user",
+		Value:    jwtToken,
+		HttpOnly: true,
+	}
+	r.AddCookie(&cookie)
+
 	_, err = w.Write([]byte("success"))
 	if err != nil {
 		log.Printf("Error while writing response at /auth/signup\n%s", err)
@@ -57,6 +89,9 @@ func validateUserSignup(user *db.User) error {
 	if match, _ := regexp.MatchString("^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$", user.Email); !match {
 		return fmt.Errorf("invalid email %s", user.Email)
 	}
-
+	// username format
+	if match, _ := regexp.MatchString("^[a-z0-9_-]{1,30}$", user.Username); !match {
+		return fmt.Errorf("invalid username %s", user.Username)
+	}
 	return nil
 }
